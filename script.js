8ef8ef7e15372a1f1b2830571eae03e0a39a713b
bethpol <bpolito001@gmail.com>
2019-06-29 11:56:38 -0400

    external script

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    particlesJS.load("particles-js", "particles.json");
+});
+
+$("#email-me").onclick(() => {
+    window.location.href = "mailto:address@gmail.com";
+});
